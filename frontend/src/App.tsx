d27ee3e190cc885e3c0b4fcc3249a5803dcc9c65
Navneeth-7989 <navneeth9966@gmail.com>
2026-03-17 19:48:24 +0530

    Create generic card component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
 
 import { Button } from "./components/Button"
+import { Card } from "./components/Card"
 
 import { PlusIcon } from "./icons/PlusIcon"
 import { ShareIcon } from "./icons/ShareIcon"
 function App() {
- 
+
 
   return (
     <>
-    
-      <Button startIcon={<PlusIcon/>} variant="primary" text={"Add content"}/>
-      <Button startIcon={<ShareIcon/>} variant="secondary" text={"Share Brain"}/>
-      
+
+      <Button startIcon={<PlusIcon />} variant="primary" text={"Add content"} />
+      <Button startIcon={<ShareIcon />} variant="secondary" text={"Share Brain"} />
+      <div className="flex gap-4">
+        <Card type="twitter" link="https://x.com/khushiirl/status/2033851811602530743" title="First tweet" />
+        <Card type="youtube" link="https://www.youtube.com/watch?v=EkZlFPhMB4E" title="First video" />
+      </div>
     </>
   )
 }
